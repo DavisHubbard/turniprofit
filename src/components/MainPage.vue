1415525a9a38c856c80975aaa8d20f41fee9c10b
DavisHubbard <davishubbard1@gmail.com>
2020-03-25 15:00:42 -0400

    Change graph library (adds filtering by user)

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="hello">
-    <GChart type="AreaChart" :data="turnipData" :options="graphOptions">
-    </GChart>
+    <apexchart
+      width="1900"
+      height="800"
+      type="line"
+      :options="options"
+      :series="series"
+    ></apexchart>
   </div>
 </template>
 
 <script>
-import { GChart } from "vue-google-charts";
+import VueApexCharts from "vue-apexcharts";
 export default {
   name: "MainPage",
   components: {
-    GChart
+    apexchart: VueApexCharts
   },
   data: function() {
     return {
-      turnipData: [["Day"]],
-      graphOptions: {
-        title: "Get Rich Quick",
-        vAxis: { minValue: 0 }
-      },
+      turnipData: {},
       idToUser: {
         UM762HAKZ: "Willard",
         U0108CJ77JN: "Hayden",
         ULXAU74AV: "Davis",
         ULWJ61PQ9: "Shoeman",
-        UM532106A: "Max"
+        UM532106A: "Max",
+        U010T1GNHP1: "Laura",
+        UM5RY5UDU: "Adam"
       },
-      orderedUsers: [
-        "UM762HAKZ",
-        "U0108CJ77JN",
-        "ULXAU74AV",
-        "ULWJ61PQ9",
-        "UM532106A"
-      ]
+      options: {
+        chart: {
+          zoom: { enabled: false },
+          animations: { enabled: false }
+        },
+        title: {
+          text: "Turnup for Turnips",
+          align: "left"
+        }
+      },
+      series: []
     };
   },
   mounted() {
     fetch("https://turniprofit.firebaseio.com/prices.json").then(response => {
       response.json().then(res => {
-        console.log(res);
         for (let userId in this.idToUser) {
-          this.turnipData[0].push(this.idToUser[userId]);
+          this.turnipData[userId] = [];
         }
         let days = [];
         for (let day in res) {
@@ -52,35 +58,46 @@ export default {
           if (a.date > b.date) return 1;
           else return -1;
         });
+        let xDays = [];
         console.log("Sorted Days: ", days);
         for (let i = 0; i < days.length; i++) {
           let date = days[i].date;
           if ("morning" in days[i]) {
-            let mornPrices = [date + " M"];
-            for (let j = 0; j < this.orderedUsers.length; j++) {
-              let userId = this.orderedUsers[j];
+            let mornName = date + " M";
+            xDays.push(mornName);
+            for (let userId in this.idToUser) {
               if (userId in days[i].morning) {
-                mornPrices.push(parseInt(days[i].morning[userId].price));
+                this.turnipData[userId].push(
+                  parseInt(days[i].morning[userId].price)
+                );
               } else {
-                mornPrices.push(0);
+                this.turnipData[userId].push(null);
               }
             }
-            this.turnipData.push(mornPrices);
-            console.log("Morning Prices: ", mornPrices);
           }
           if ("evening" in days[i]) {
-            let evePrices = [date + " E"];
-            for (let j = 0; j < this.orderedUsers.length; j++) {
-              let userId = this.orderedUsers[j];
+            let eveName = date + " E";
+            xDays.push(eveName);
+            for (let userId in this.idToUser) {
               if (userId in days[i].evening) {
-                evePrices.push(parseInt(days[i].evening[userId].price));
+                this.turnipData[userId].push(
+                  parseInt(days[i].evening[userId].price)
+                );
               } else {
-                evePrices.push(0);
+                this.turnipData[userId].push(null);
               }
             }
-            this.turnipData.push(evePrices);
-            console.log("Evening Prices: ", evePrices);
           }
+          this.options = {
+            ...this.options,
+            ...{ xaxis: { categories: xDays } }
+          };
+        }
+        for (let userId in this.turnipData) {
+          this.series.push({
+            name: this.idToUser[userId],
+            data: this.turnipData[userId]
+          });
         }
       });
     });
