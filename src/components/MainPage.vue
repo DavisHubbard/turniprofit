a4d78e7a27ad545dcd9d34a2fc29259bb4c9d3d6
DavisHubbard <davishubbard1@gmail.com>
2020-03-25 23:06:59 -0400

    Updated user list population method

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -19,19 +19,21 @@ export default {
   },
   data: function() {
     return {
+      TOKEN: "xoxb-710610623249-1022992123159-pmB0iVAoSO6r6crIhmXvKRZR",
+      bot: null,
       turnipData: {},
       idToUser: {
-        UM762HAKZ: "Willard",
-        U0108CJ77JN: "Hayden",
-        ULXAU74AV: "Davis",
-        ULWJ61PQ9: "Shoeman",
-        UM532106A: "Max",
-        U010T1GNHP1: "Laura",
-        UM5RY5UDU: "Adam",
-        UN4N71EMB: "Alex Wong",
-        ULT0JB92P: "Alex J",
-        URGUP061Y: "Drew",
-        U010F1DSQ6P: "Bo"
+        // UM762HAKZ: "Willard",
+        // U0108CJ77JN: "Hayden",
+        // ULXAU74AV: "Davis",
+        // ULWJ61PQ9: "Shoeman",
+        // UM532106A: "Max",
+        // U010T1GNHP1: "Laura",
+        // UM5RY5UDU: "Adam",
+        // UN4N71EMB: "Alex Wong",
+        // ULT0JB92P: "Alex J",
+        // URGUP061Y: "Drew",
+        // U010F1DSQ6P: "Bo"
       },
       options: {
         chart: {
@@ -47,64 +49,86 @@ export default {
     };
   },
   mounted() {
-    fetch("https://turniprofit.firebaseio.com/prices.json").then(response => {
-      response.json().then(res => {
-        for (let userId in this.idToUser) {
-          this.turnipData[userId] = [];
-        }
-        let days = [];
-        for (let day in res) {
-          let obj = res[day];
-          obj["date"] = day;
-          days.push(obj);
-        }
-        days.sort((a, b) => {
-          if (a.date > b.date) return 1;
-          else return -1;
-        });
-        let xDays = [];
-        console.log("Sorted Days: ", days);
-        for (let i = 0; i < days.length; i++) {
-          let date = days[i].date;
-          if ("morning" in days[i]) {
-            let mornName = date + " M";
-            xDays.push(mornName);
-            for (let userId in this.idToUser) {
-              if (userId in days[i].morning) {
-                this.turnipData[userId].push(
-                  parseInt(days[i].morning[userId].price)
-                );
-              } else {
-                this.turnipData[userId].push(null);
-              }
-            }
+    const Slack = require("slack");
+    const token = this.TOKEN;
+    this.bot = new Slack({ token });
+    this.bot.conversations
+      .members({ channel: "C010KUBA9V5" })
+      .then(this.buildUserList)
+      .catch(console.error);
+  },
+  methods: {
+    getPriceData: function() {
+      fetch("https://turniprofit.firebaseio.com/prices.json").then(response => {
+        response.json().then(res => {
+          for (let userId in this.idToUser) {
+            this.turnipData[userId] = [];
           }
-          if ("evening" in days[i]) {
-            let eveName = date + " E";
-            xDays.push(eveName);
-            for (let userId in this.idToUser) {
-              if (userId in days[i].evening) {
-                this.turnipData[userId].push(
-                  parseInt(days[i].evening[userId].price)
-                );
-              } else {
-                this.turnipData[userId].push(null);
-              }
-            }
+          let days = [];
+          for (let day in res) {
+            let obj = res[day];
+            obj["date"] = day;
+            days.push(obj);
           }
-          this.options = {
-            ...this.options,
-            ...{ xaxis: { categories: xDays } }
-          };
-        }
-        for (let userId in this.turnipData) {
-          this.series.push({
-            name: this.idToUser[userId],
-            data: this.turnipData[userId]
+          days.sort((a, b) => {
+            if (a.date > b.date) return 1;
+            else return -1;
           });
-        }
+          let xDays = [];
+          console.log("Sorted Days: ", days);
+          for (let i = 0; i < days.length; i++) {
+            let date = days[i].date;
+            if ("morning" in days[i]) {
+              let mornName = date + " M";
+              xDays.push(mornName);
+              for (let userId in this.idToUser) {
+                if (userId in days[i].morning) {
+                  this.turnipData[userId].push(
+                    parseInt(days[i].morning[userId].price)
+                  );
+                } else {
+                  this.turnipData[userId].push(null);
+                }
+              }
+            }
+            if ("evening" in days[i]) {
+              let eveName = date + " E";
+              xDays.push(eveName);
+              for (let userId in this.idToUser) {
+                if (userId in days[i].evening) {
+                  this.turnipData[userId].push(
+                    parseInt(days[i].evening[userId].price)
+                  );
+                } else {
+                  this.turnipData[userId].push(null);
+                }
+              }
+            }
+            this.options = {
+              ...this.options,
+              ...{ xaxis: { categories: xDays } }
+            };
+          }
+          for (let userId in this.turnipData) {
+            this.series.push({
+              name: this.idToUser[userId],
+              data: this.turnipData[userId]
+            });
+          }
+        });
       });
-    });
+    },
+    buildUserList: function(res) {
+      for (let i = 0; i < res.members.length; i++) {
+        this.bot.users.info({ user: res.members[i] }).then(resp => {
+          console.log(resp);
+          if (resp.user.real_name !== "turniprofit") {
+            this.idToUser[resp.user.id] = resp.user.real_name;
+          }
+        });
+      }
+      this.getPriceData();
+    }
   }
 };
 </script>
